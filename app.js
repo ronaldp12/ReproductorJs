5d3e7a0955197cb78ba03037e49bf2355559fc7b
Ronald patiño <patinoguevararonald@gmail.com>
2024-10-23 08:58:21 -0500

    imagenes y audio subidos

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import { playList } from "./playlist.js"
+
 const audio =document.getElementById('audio')
 const play =document.getElementById('play')
 const pause =document.getElementById('pause')
 const forward= document.getElementById('forward')
 const rewind= document.getElementById('rewind')
 const stop= document.getElementById('stop')
+const skipPrevious= document.getElementById('skip-previous')
+const skipNext= document.getElementById('skip-next')
 
 play.addEventListener('click', () =>audio.play())
 
@@ -16,4 +20,10 @@ forward.addEventListener('click', () => audio.currentTime +=10)
 stop.addEventListener('click',() => {
     audio.pause()
     audio.currentTime= 0
+})
+
+skipNext.addEventListener('click', () => {
+    for (let i = 0; i < playList.length; i++) {
+        playList.play()
+    }
 })
---
+++ playlist.js
@@ -0,0 +1,34 @@
+export {playList}
+
+const playList = [
+    {
+      title: 'Lloraras',
+      artist: 'Oscar de Leon',
+      img: '/assets/images/lloraras.jpg',
+      song: '/assets/sounds/Lloraras.mp3'
+    },
+    {
+      title: 'Busca por Dentro',
+      artist: 'Grupo Niche',
+      img: 'assets/images/busca-por-dentro.jpg',
+      song: '/assets/sounds/Busca por Dentro.mp3'
+    },
+    {
+      title: 'Conteo Regresivo',
+      artist: 'Gilberto Santa Rosa',
+      img: 'assets/images/conteo-regresivo.jpg',
+      song: '/assets/sounds/Conteo Regresivo.mp3'
+    },
+    {
+      title: 'Fabricando Fantasias',
+      artist: 'Tito Nieves',
+      img: 'assets/images/fabricando-fantasias.jpeg',
+      song: '/assets/sounds/Fabricando Fantasías.mp3'
+    },
+    {
+      title: 'Una Aventura',
+      artist: 'Grupo Niche',
+      img: 'assets/images/una-aventura.jpg',
+      song: '/assets/sounds/Una Aventura.mp3'
+    },
+  ]
